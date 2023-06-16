207e7f1e211f8196e605730aa3bb9336db3e05e2
Charles Beam <charles.beam@bryx.com>
2023-06-15 19:04:45 -0500

    topbar

---
+++ src/topbar/Topbar.tsx
@@ -0,0 +1,9 @@
+const Topbar = () => {
+  return (
+    <div className="w-[1280px] h-16 p-0 bg-green-200 flex items-center justify-start">
+      <h2 className="ml-4">Dashboard++</h2>
+    </div>
+  );
+};
+
+export default Topbar;
